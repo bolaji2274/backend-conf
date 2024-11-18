0249ba3f13caeed100c1c258f9d7e9ecf3a8dee1
bolaji2274 <hammedbolajihammed@gmail.com>
2024-11-18 18:20:27 +0100

    file services

--- frontend/src/pages/Services.js
+++ frontend/src/pages/Services.js
@@ -8,41 +8,7 @@ import servicesImage from '../assets/images/chicken-3.jpg'
 function Services() {
   return (
     <div>
-            {/* Services Section */}
-      {/* <Container maxWidth="lg" className="services-section">
-        <Typography variant="h4" gutterBottom className='text-center'>
-          Our Services
-        </Typography>
-        <Grid container spacing={4}>
-          <Grid item xs={12} sm={6} md={4}>
-            <Box className="service-box">
-              <Typography variant="h6">Livestock Provision</Typography>
-              <Typography variant="body2" paragraph>
-                We provide high-quality broilers, layers, fish, and other livestock to farmers, ensuring
-                that they receive the healthiest and most productive stock.
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Box className="service-box">
-              <Typography variant="h6">Feed and Supplies</Typography>
-              <Typography variant="body2" paragraph>
-                Our farm provides premium feeds and necessary supplies for your livestock, making sure
-                they grow healthy and strong.
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Box className="service-box">
-              <Typography variant="h6">Farm Management Support</Typography>
-              <Typography variant="body2" paragraph>
-                We assist farmers with farm management, offering expert advice, training, and resources
-                to ensure successful operations.
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-      </Container> */}
+   
 
 
                   {/* Services Section */}
